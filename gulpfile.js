61b1b7cba18a0214292bc91a646d26581505a396
Joseph Ross <jross@virtualinc.com>
2018-04-24 12:11:01 -0400

    Commit message

--- gulpfile.js
+++ gulpfile.js
@@ -19,43 +19,11 @@ gulp.task('concat-css', function () {
         .pipe(gulp.dest('build'))
 });
 
-gulp.task('check-message', function () {
-    if (!argv.m) {
-        var err = new PluginError('test', {
-            message: '\n\nPLEASE INCLUDE COMMIT MESSAGE\nExample: gulp -m "commit message"'
-        });
-        throw (err);
-    }
-});
-
-gulp.task('git-add', function () {
-    return gulp.src('./*')
-    .pipe(git.add({quiet: true}));
-});
-
-gulp.task('git-commit', function () {
-    return gulp.src('./*')
-    .pipe(git.commit(argv.m));
-});
-
-gulp.task('git-push', function () {
-    git.push('origin', 'master', function (err) {
-        if (err){
-            console.log('err')
-            console.log(err)
-            throw err;
-        }
-    });
-});
 
 gulp.task('default', function (callback) {
     runSequence(
-        'concat-css',
         'clear-build',
-        'check-message',
-        'git-add',
-        // 'git-commit',
-        // 'git-push',
+        'concat-css',
         function (error) {
             if (error) {
                 console.log(error.message);
