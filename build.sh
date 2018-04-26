151a7334b3365582565b21d902358967c4ca307f
Joseph Ross <jross@virtualinc.com>
2018-04-26 13:55:11 -0400

    Cleanup

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-if [ $1 != ""]
+if [ -z "$1" ]
 then
+printf "\nERROR:\nYOU MUST SUPPLY GIT COMMIT MESSAGE\n"
+else 
 gulp
 commit_message="$1"
 git add . -A
 git commit -m "$commit_message"
 git push origin master
-else 
-echo \n\nERROR:\nYOU MUST SUPPLY GIT COMMIT MESSAGE
 fi
