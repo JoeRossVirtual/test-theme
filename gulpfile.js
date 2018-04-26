4a4ef21879e662962c68e0b717b93e46c5cbcee9
Joseph Ross <jross@virtualinc.com>
2018-04-26 13:25:15 -0400

    $1

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,30 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var concatCss = require('gulp-concat-css');
 var del = require('del');
-var argv = require('yargs').argv;
-var PluginError = require('plugin-error');
 var git = require('gulp-git');
+var cleanCSS = require('gulp-clean-css');
 
-
+// Empties the build folder
 gulp.task('clear-build', function () {
     return del([
         './build/**/*',
     ]);
 });
 
-gulp.task('concat-css', function () {
+gulp.task('build-css', function () {
     gulp.src('./src/css/**/*.css')
+        // Concatinates CSS files
         .pipe(concatCss("style.css"))
+        // Minifies CSS
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('build'))
 });
 
-
+// Runs tasks
 gulp.task('default', function (callback) {
     runSequence(
         'clear-build',
-        'concat-css',
+        'build-css',
         function (error) {
             if (error) {
                 console.log(error.message);
