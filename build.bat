7794071d53a1ebce92ab8e4e20f7ccd80cd41040
Joseph Ross <jross@virtualinc.com>
2018-04-27 10:39:29 -0400

    Refactor build script

--- build.bat
+++ build.bat
@@ -1,12 +1,17 @@
 :TOP
 IF "%~1"=="" (
-    ECHO.
-    ECHO ERROR:
-    ECHO YOU MUST SUPPLY A GIT COMMIT MESSAGE
-    ECHO.
+   call :ERROR
 ) ELSE (
-    call gulp
-    call git add . -A
-    git commit -m %1
-    git push origin master
+    call :BUILD %1
 )
+
+:ERROR
+ECHO ERROR:   YOU MUST SUPPLY A GIT COMMIT MESSAGE
+EXIT /B 0
+
+:BUILD
+call gulp
+call git add . -A
+call git commit -m "%~1"
+call git push origin master
+EXIT /B 0
