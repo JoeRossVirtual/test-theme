e55af24a871bba531d7fba9d408a3451560b5dd8
Joseph Ross <jross@virtualinc.com>
2018-04-27 11:35:10 -0400

    Change to js

--- src/js/test-page.js
+++ src/js/test-page.js
@@ -1,9 +1,14 @@
 jQuery("img").hide();
+console.log("I ran");
 let first = jQuery(".first");
 var color = ["red", "orange", "yellow", "green", "blue", "purple"];
 let i = 0;
-setTimeout(function(){
-    //do what you need here
-    i = (i+1)%color.length;
-    first.css("background-color: " + color[i])
-}, 1000);
+while(true){
+    setTimeout(function(){
+        //do what you need here
+        i = (i+1)%color.length;
+        console.log(i);
+        first.css("background-color: " + color[i]);
+        console.log(first);
+    }, 1000);
+}
