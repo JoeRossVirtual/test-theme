b22dafb9f6ff7a7b51d3900e6efa491ed7891980
Joseph Ross <jross@virtualinc.com>
2018-04-30 11:28:42 -0400

    Test

--- build/js/test-page.js
+++ build/js/test-page.js
@@ -1,1 +1,1 @@
-jQuery("img").hide(),console.log("I ran");let first=jQuery(".first");var color=["red","orange","yellow","green","blue","purple"];let i=0;first.html("This is a staging test."),setInterval(function(){first.css("background-color",color[i]),i=(i+1)%color.length},1e3);
+jQuery("img").hide(),console.log("I ran");let first=jQuery(".first");var color=["red","orange","yellow","green","blue","purple"];let i=0;first.html("This is a staging test 1."),setInterval(function(){first.css("background-color",color[i]),i=(i+1)%color.length},1e3);
